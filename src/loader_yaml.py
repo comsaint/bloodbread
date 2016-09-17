0a6bba20b8cbda4ba23ec81912796df926d02641
Long Pun <longpun88@gmail.com>
2016-09-17 23:54:35 +0800

    Update data reader for person category

--- src/loader_yaml.py
+++ src/loader_yaml.py
@@ -1,5 +1,6 @@
 #import basic packages
 import os
+import pprint
 
 #import special packages
 import yaml
@@ -25,31 +26,38 @@ class ReadData():
         
     def readRawData(self):
         # Loop all things in DATAPATH
-        for entry in os.scandir(RAWDATAPATH):
+        for datapath in os.scandir(RAWDATAPATH):
             # If we find an interesting category...
-            if entry.is_dir() and entry.name in DATACATEGORIES.keys():
+            if datapath.is_dir() and datapath.name in DATACATEGORIES.keys():
                 # Add a new key to dataholder, which contains an empty list to be assigned
-                self.dataholder[entry.name] = []
-                # Then loop over all subfolders inside the category
-                for entity in os.scandir(entry.path):
-                    # Put content into dataholder
-                    if entry.name == 'person':
-                        ## Each 'person' entry is a folder, contains a 'brief.yaml' and (optional) 'portrait.png'
-                        # Create a dict with the person's name as key
-                        person_dict = {entity.name:{}}
-                        
+                category_name = datapath.name
+                temp_list = []
+                # Put content into dataholder
+                if category_name == 'person':
+                    ## Each 'person' entry is a folder, contains a 'brief.yaml' and (sometimes) 'portrait.png'
+                    # Then loop over all subfolders inside the category
+                
+                    for entity in os.scandir(datapath.path):
+                        #print(entity)
+                        person_dict = {}
                         # Loop the person's folder
                         for doc in os.scandir(entity.path):
-                            if doc.name in DATACATEGORIES[entry.name] and '.yaml' in doc.name:
-                                person_dict[entity.name]['Brief'] = yaml.safe_load(open(entity.path+'/'+DATACATEGORIES[entry.name][i],'r'))
-                            #elif '.png' in DATACATEGORIES[entry.name][i]:
-                            #    person_dict[entity.name]['Portrait'] = entity.path+'/'+DATACATEGORIES[entry.name][i]
-                            #else:
-                            #    break
-                    elif entry.name == 'family':
-                        pass
-                    elif entry.name == 'company':
-                        pass
+                            if doc.name == 'brief.yaml':
+                                #print(doc.path)
+                                person_dict.update(yaml.safe_load(open(doc.path,'r')))
+                            elif doc.name == 'portrait.png':
+                                person_dict.update({'Portrait' : doc.path})
+                        temp_list.append(person_dict)
+                    self.dataholder[category_name] = temp_list
+                    #pprint.pprint(temp_list)
+                    del temp_list
+                    del person_dict
+                elif category_name == 'family':
+                    pass
+                elif category_name == 'company':
+                    pass
+                else:
+                    print("Warning: unknown data folder name {0}").format(datapath.name)
                     
     """    
     with open("/home/long/Desktop/projects/bloodbread/src/test.yaml") as file:
